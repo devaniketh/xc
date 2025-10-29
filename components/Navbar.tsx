dd547f693da7948d234e39f194d8ad7238840305
aniketh <techrunstheworld@gmail.com>
2025-10-29 19:50:47 +0530

    chore: update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,57 +4,19 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <div className="relative">
-
+        <nav className="w-full px-6 py-6 flex flex-col sm:flex-row sm:items-center sm:justify-between relative z-20">
             <h1
-                className="text-[40px] font-semibold leading-[100%] tracking-[0%]
-                   bg-linear-to-r from-[#CCD1E9] to-[#CACFE7]
-                   bg-clip-text text-transparent font-museoModerno
-                   absolute top-[23px] left-[46px] opacity-100"
+                className="text-[32px] sm:text-[40px] font-semibold bg-linear-to-r from-[#CCD1E9] to-[#CACFE7] bg-clip-text text-transparent font-museoModerno"
             >
                 xCombinator
             </h1>
-
-
-            <div
-                className="absolute top-[100px] left-[46px] w-[1636px] h-0
-                   border border-[#FFFFFF1A] opacity-100"
-            />
-
-
-            <p
-                className="absolute top-[46px] left-[1162px] text-right
-                   text-[14px] font-medium leading-[100%]
-                   text-[#636475] font-robotoMono opacity-100"
-            >
-                ambassador
-            </p>
-
-
-            <p
-                className="absolute top-[46px] left-[1314px] text-right
-                   text-[14px] font-medium leading-[100%]
-                   text-[#636475] font-robotoMono opacity-100"
-            >
-                our products
-            </p>
-
-
-            <p
-                className="absolute top-[46px] left-[1474px] text-right
-                   text-[14px] font-medium leading-[100%]
-                   text-[#636475] font-robotoMono opacity-100"
-            >
-                $X
-            </p>
-
-            <p
-                className="absolute top-[46px] left-[1527px] text-right
-                   text-[14px] font-medium leading-[100%]
-                   text-[#636475] font-robotoMono opacity-100"
-            >
-                contact us
-            </p>
-        </div>
+            <div className="flex flex-wrap gap-6 sm:gap-10 text-[14px] font-robotoMono text-[#636475] mt-4 sm:mt-0">
+                <p className="cursor-pointer hover:text-white">ambassador</p>
+                <p className="cursor-pointer hover:text-white">our products</p>
+                <p className="cursor-pointer hover:text-white">$X</p>
+                <p className="cursor-pointer hover:text-white">contact us</p>
+            </div>
+            <div className="absolute bottom-0 left-0 w-full border border-[#FFFFFF1A]" />
+        </nav>
     )
 }
